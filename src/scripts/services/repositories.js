81ae741601576ab645e7a9bd02a92f5a9b76da81
Uanderson Lima <Uanderson.lm@gmail.com>
2023-09-02 16:14:50 -0300

    Projeto-GitHub

--- src/scripts/services/repositories.js
+++ src/scripts/services/repositories.js
@@ -1,4 +1,4 @@
-import {baseUrl} from "/src/scripts/variables.js"
+import {baseUrl} from '../variables.js'
 
 async function getRepositories(userName) {
     const response = await fetch(`${baseUrl}/${userName}/repos`)
